2f917833b19dcf08e0d143b6f8035ec6ed080a83
Davide De Masi <demasi.dav@gmail.com>
2025-11-06 17:57:50 +0100

    creazione navbar.js per organizzazione codice

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,4 @@
-//eventListener per voce selezionata nella Navbar
-const nav = document.querySelector('.navbar');
-nav.addEventListener('click', (e) => {
-    
-  if (e.target.tagName === 'A') {
-    e.preventDefault();
-    nav.querySelectorAll('a').forEach(element => element.classList.remove('selected'));
-    e.target.classList.add('selected');
-  }
-  if (e.target.dataset.page === 'new'){
-    loadnews();
-  }
-});
- 
+
 //funzione per creare card
 const parent = document.querySelector('.news');
 function createCard(by,title,url,score,comm){
---
+++ src/js/navbar.js
@@ -0,0 +1,13 @@
+//eventListener per voce selezionata nella Navbar
+const nav = document.querySelector('.navbar');
+nav.addEventListener('click', (e) => {
+    
+  if (e.target.tagName === 'A') {
+    e.preventDefault();
+    nav.querySelectorAll('a').forEach(element => element.classList.remove('selected'));
+    e.target.classList.add('selected');
+  }
+  if (e.target.dataset.page === 'new'){
+    loadnews();
+  }
+});
