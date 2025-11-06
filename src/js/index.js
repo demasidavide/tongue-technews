de50c0ea15cb92b56e3fabfe79af6156ff1c207f
Davide De Masi <demasi.dav@gmail.com>
2025-11-06 09:27:22 +0100

    definito card e pulsanti per notizie con pulsanti

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,25 @@
+//eventListener per voce selezionata nella Navbar
+const nav = document.querySelector('.navbar');
+nav.addEventListener('click', (e) => {
+    e.preventDefault();
+  if (e.target.tagName === 'A') {
+    nav.querySelectorAll('a').forEach(element => element.classList.remove('selected'));
+    e.target.classList.add('selected');
+  }
+});
+ 
+//funzione per creare card
 
+
+
+//prova di chiamata news
+
+
+const apiBase= 'https://hacker-news.firebaseio.com/v0/'
+
+fetch(apiBase + 'item/1345.json')
+.then(response=>response.json())
+.then(data=>{
+  console.log(data)
+
+})
