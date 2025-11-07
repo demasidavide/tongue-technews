bfe8a1b58a6fd48f55c59e85761e2476aeeb5218
Davide De Masi <demasi.dav@gmail.com>
2025-11-07 15:03:41 +0100

    modifica a navbar.js per prevent default di search

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -3,7 +3,11 @@ const nav = document.querySelector('.navbar');
 nav.addEventListener('click', (e) => {
     
   if (e.target.tagName === 'A') {
-    e.preventDefault();
+    //e.preventDefault();
+    if(!e.target.getAttribute('href').startWith('#')){
+      e.preventDefault();
+    }
+
     nav.querySelectorAll('a').forEach(element => element.classList.remove('selected'));
     e.target.classList.add('selected');
   }
