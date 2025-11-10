a87fcb0c56059eae8a1661e3b7dfd2c4f654769a
Davide De Masi <demasi.dav@gmail.com>
2025-11-10 18:17:03 +0100

    creazione favorites.js

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,4 +1,32 @@
-//eventListener per voce selezionata nella Navbar
+
+const title = document.querySelector('.title')
+  const sectionTop = document.querySelector('.top-news')
+  const sectionNews = document.querySelector('.news')
+  const containerLoad = document.querySelector('.container-load')
+  const sectionSearch = document.querySelector('.search-news')
+
+  function newClick(){
+    title.textContent = 'Le Top News'
+    sectionTop.style.display ='block';
+    sectionNews.style.display ='block';
+    containerLoad.style.display ='block';
+    sectionSearch.style.display ='block';
+  }
+  export function favoritesClick(){
+    const title = document.querySelector('.title')
+  const sectionTop = document.querySelector('.top-news')
+  const sectionNews = document.querySelector('.news')
+  const containerLoad = document.querySelector('.container-load')
+  const sectionSearch = document.querySelector('.search-news')
+    title.textContent = 'I miei Preferiti'
+    sectionTop.style.display ='none';
+    sectionNews.style.display ='none';
+    containerLoad.style.display ='none';
+    sectionSearch.style.display ='none';
+
+  }
+
+
 const nav = document.querySelector('.navbar');
 nav.addEventListener('click', (e) => {
     console.log(e)
@@ -13,22 +41,11 @@ nav.addEventListener('click', (e) => {
     e.target.classList.add('selected');
   }
   if (e.target.dataset.page === 'new'){
-    loadnews();
+      newClick();
+      loadnews();
   }
-
-  const title = document.querySelector('.title')
-  const sectionTop = document.querySelector('.top-news')
-  const sectionNews = document.querySelector('.news')
-  const containerLoad = document.querySelector('.container-load')
-  const sectionSearch = document.querySelector('.search-news')
   if(e.target.id === 'favorites'){
-    title.textContent = 'I miei Preferiti'
-    sectionTop.style.display ='none';
-    sectionNews.style.display ='none';
-    containerLoad.style.display ='none';
-    sectionSearch.style.display ='none';
+    favoritesClick();
     }
-
-
 
 });
