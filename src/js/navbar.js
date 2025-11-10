673951f1c2f49def311cb3f3691ada553836f1c6
Davide De Masi <demasi.dav@gmail.com>
2025-11-10 17:48:28 +0100

    creazione sezione favorites

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,10 +1,11 @@
 //eventListener per voce selezionata nella Navbar
 const nav = document.querySelector('.navbar');
 nav.addEventListener('click', (e) => {
-    
+    console.log(e)
   if (e.target.tagName === 'A') {
     //e.preventDefault();
-    if(!e.target.getAttribute('href').startWith('#')){
+    
+    if(!e.target.getAttribute('href').startsWith('#')){
       e.preventDefault();
     }
 
@@ -14,4 +15,20 @@ nav.addEventListener('click', (e) => {
   if (e.target.dataset.page === 'new'){
     loadnews();
   }
+
+  const title = document.querySelector('.title')
+  const sectionTop = document.querySelector('.top-news')
+  const sectionNews = document.querySelector('.news')
+  const containerLoad = document.querySelector('.container-load')
+  const sectionSearch = document.querySelector('.search-news')
+  if(e.target.id === 'favorites'){
+    title.textContent = 'I miei Preferiti'
+    sectionTop.style.display ='none';
+    sectionNews.style.display ='none';
+    containerLoad.style.display ='none';
+    sectionSearch.style.display ='none';
+    }
+
+
+
 });
